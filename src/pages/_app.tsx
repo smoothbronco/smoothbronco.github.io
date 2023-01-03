db8692275bd66a5518345a68b69d6d2197cd098c
smoothbronco <884takahiro63@gmail.com>
2023-01-03 16:42:52 +0900

    update: pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import '@/styles/index.css';
+import { Footer } from '@/components/features/app/Footer';
+import { Header } from '@/components/features/app/Header';
+import { ContentLayout } from '@/components/features/app/Layout';
+import { Seo } from '@/components/features/app/Seo';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Seo />
+
+      <div
+        className="grid gap-10 min-h-screen bg-global"
+        style={{ gridTemplateRows: 'auto 1fr auto' }}
+      >
+        <Header />
+        <ContentLayout className="px-0 py-6 sm:p-6">
+          <Component {...pageProps} />
+        </ContentLayout>
+        <Footer />
+      </div>
+    </>
+  );
 }
